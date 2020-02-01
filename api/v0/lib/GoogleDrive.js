2a96fbc4d1f8ff749a2112c644c1fd324e2fc009
clinton <clinton.law@propelplm.com>
2020-02-01 13:48:06 -0800

    refactor code to accommodate other external file storage platforms

---
+++ api/v0/lib/GoogleDrive.js
@@ -0,0 +1,68 @@
+const { google } = require("googleapis");
+const fs = require("fs");
+
+const redirect_uris = ["urn:ietf:wg:oauth:2.0:oob", "http://localhost"];
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+async function authorize(
+  clientId,
+  clientSecret,
+  tokens,
+  fileName,
+  mimeType,
+  callback
+) {
+  const oAuth2Client = new google.auth.OAuth2(
+    clientId,
+    clientSecret,
+    redirect_uris[0]
+  );
+  oAuth2Client.setCredentials(tokens);
+  return await callback(oAuth2Client, fileName, mimeType);
+}
+
+async function uploadFile(auth, fileName, mimeType) {
+  var fileMetadata = {
+    name: fileName
+  };
+  try {
+    const drive = google.drive({ version: "v3", auth });
+    const fileStream = fs.createReadStream(`./${fileName}`);
+    var media = {
+      mimeType: mimeType,
+      body: fileStream
+    };
+    const file = await drive.files.create({
+      resource: fileMetadata,
+      media
+    });
+    const response = {
+      status: file.status,
+      data: file.data
+    };
+    return sendSuccessResponse(response, "uploadFile");
+  } catch (err) {
+    return sendErrorResponse(err, "uploadFile");
+  }
+}
+
+function sendSuccessResponse(response, functionName) {
+  console.log(
+    `${functionName} has succeeded with response: ${JSON.stringify(response)}.`
+  );
+  return response;
+}
+
+function sendErrorResponse(error, functionName) {
+  console.log(`${functionName} has failed due to error: ${error}.`);
+  return error;
+}
+
+module.exports = {
+  authorize,
+  uploadFile
+};
---
+++ api/v0/main.js
@@ -0,0 +1,99 @@
+"use-strict";
+
+const express = require("express");
+const app = express();
+
+const util = require("util");
+const multer = require("multer");
+const cors = require("cors");
+const path = require("path");
+const GoogleDrive = require("./lib/GoogleDrive.js");
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static(path.join(__dirname, "../../public")));
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "../../public/index.html");
+});
+
+var client_id;
+var client_secret;
+var tokensFromCredentials;
+app.post("/token", (req, res) => {
+  try {
+    ({
+      client_secret,
+      client_id,
+      access_token,
+      refresh_token,
+      expiry_date
+    } = req.body);
+
+    tokensFromCredentials = {
+      access_token,
+      refresh_token,
+      scope: "https://www.googleapis.com/auth/drive.file",
+      token_type: "Bearer",
+      expiry_date
+    };
+    sendSuccessResponse(tokensFromCredentials, "/tokens endpoint");
+    res.status(200).send(tokensFromCredentials);
+  } catch (err) {
+    sendErrorResponse(err, "/tokens endpoint");
+    res.send(`Failed to receive tokens: ${err}`);
+  }
+});
+
+app.post("/upload", async (req, res) => {
+  var fileName;
+  var mimeType;
+  var storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+      cb(null, "./");
+    },
+    filename: function(req, file, cb) {
+      fileName = file.originalname;
+      mimeType = file.mimetype;
+      cb(null, fileName);
+    }
+  });
+  var upload = util.promisify(multer({ storage: storage }).single("file"));
+  // Load client secrets from a local file.
+  try {
+    await upload(req, res);
+  } catch (err) {
+    console.log(`Upload from local failed with ${err}`);
+  }
+  try {
+    // Authorize a client with credentials, then call the Google Drive API.
+    const response = await GoogleDrive.authorize(
+      client_id,
+      client_secret,
+      tokensFromCredentials,
+      fileName,
+      mimeType,
+      GoogleDrive.uploadFile
+    );
+    res.status(response.status).send(response.data);
+  } catch (err) {
+    res.status(503).send(`Drive upload failed: ${err}`);
+  }
+});
+
+app.listen(port, () => {
+  console.log("Endpoints ready.");
+});
+
+function sendSuccessResponse(response, functionName) {
+  console.log(
+    `${functionName} has succeeded with response: ${JSON.stringify(response)}.`
+  );
+  return response;
+}
+
+function sendErrorResponse(error, functionName) {
+  console.log(`${functionName} has failed due to error: ${error}.`);
+  return error;
+}
