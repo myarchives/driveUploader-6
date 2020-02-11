9ed3aa8b30ff4c4b416c15326b6e4cfcf8b58a86
clinton <clinton.law@propelplm.com>
2020-02-11 14:14:25 -0800

    try upload progress

--- api/v0/lib/GoogleDrive.js
+++ api/v0/lib/GoogleDrive.js
@@ -36,7 +36,7 @@ async function uploadFile(auth, fileName, mimeType) {
       mimeType: mimeType,
       body: fileStream
     };
-    const req = await drive.files.insert(
+    const req = await drive.files.create(
       {
         resource: fileMetadata,
         media
