bf0fa83bc7196d6bea1b37dc0a848f8dd557f6ea
clinton <clinton.law@propelplm.com>
2020-02-13 17:25:02 -0800

    fix reference to obj

--- api/v0/lib/JsForce.js
+++ api/v0/lib/JsForce.js
@@ -14,8 +14,11 @@ async function connect(sessionId, salesforceUrl) {
 
 function create() {
   connection
-    .sobject("Account")
-    .create({ Name: "Another Account" })
+    .sobject("PLMLAW__Document__c")
+    .create({
+      Name: "Test Document",
+      PLMLAW__Item_Revision__c: "a0V6g000000KFZmEAO"
+    })
     .then(() => console.log("well done"))
     .catch(() => console.log("sike"));
 }
