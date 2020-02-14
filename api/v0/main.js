a9e5d218535f3518388ea224dfd30625bd760483
clinton <clinton.law@propelplm.com>
2020-02-13 17:20:40 -0800

    fix reference to obj

--- api/v0/main.js
+++ api/v0/main.js
@@ -103,7 +103,7 @@ app.listen(port, () => {
 
 function sendSuccessResponse(response, functionName) {
   const logEnding =
-    Object.entries(response).length === 0 && obj.constructor === Object
+    Object.entries(response).length === 0 && response.constructor === Object
       ? ""
       : `: ${JSON.stringify(response)}`;
   console.log(`${functionName} has succeeded with response${logEnding}.`);
