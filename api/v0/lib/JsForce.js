1ed6e41c779014504ded5f802b1b8b50748263eb
clinton <clinton.law@propelplm.com>
2020-02-24 18:10:03 -0800

    debug msg

--- api/v0/lib/JsForce.js
+++ api/v0/lib/JsForce.js
@@ -23,13 +23,14 @@ function create(file) {
     .sobject("PLMLAW__Document__c")
     .create({
       Name: name,
-      PLMLAW__Item_Revision__c: "a0X6g000000Jl2ZEAS", //hardcoded just for demo
+      PLMLAW__Item_Revision__c: "a0V6g000000KFZmEAO", //hardcoded just for demo
       PLMLAW__External_Attachment_URL__c: webViewLink,
       PLMLAW__File_Extension__c: fileExtension,
       PLMLAW__Google_File_Id__c: id
     })
     .then(res => console.log("well done: " + fileExtension))
     .catch(err => console.log("sike: " + err));
+  console.log('done')
 }
 
 module.exports = {
